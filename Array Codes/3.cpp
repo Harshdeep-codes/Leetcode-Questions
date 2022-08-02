b39503940effea8e44e28e3cab994e48d351eab5
Harshdeep Singh <90376919+Harshdeep-codes@users.noreply.github.com>
2022-08-02 23:54:46 +0530

    Update 3.cpp

--- Array Codes/3.cpp
+++ Array Codes/3.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        long long count = 0;        
+        for(int i=0; i<nums.size(); i++){
+            count += nums[i];
+            nums.push_back(count);
+        }
+         return nums;
+    }
+   
+};
